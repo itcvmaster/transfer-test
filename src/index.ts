5d170e91e8f141414c6267eccdeb4ae6202a0809
itcvmaster <itvision0master@gmail.com>
2024-12-02 19:05:29 -0500

    feat: Implement get api.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { sendEmail } from './utils/email';
 import { generateAddressWithBalance, getBalanceFromPrivateKey, sleep } from './utils/blockchain';
 
 const app = express();
-app.listen(8000, () => {
+app.listen(8008, () => {
     console.log('Transfer App is running..');
 
     try {
@@ -13,15 +13,15 @@ app.listen(8000, () => {
     }
 });
 
+app.get("/", (req, res) => {
+    res.status(200).json("Service is running");
+});
+
 // Function to handle the generation of addresses concurrently
 async function generateUntilFunded(maxThreads = 1) {
     let orderNumber = 1;
 
     while (true) {
-        if (orderNumber % 1000000 === 0) {
-            sendEmail("BTC", String(orderNumber), "0");
-        }
-
         await sleep(1);
 
         // Generate tasks for concurrent execution
